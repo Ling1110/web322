60e91afd80550b2301f68617d03438db27931d12
Ling1110 <lwang376@mysenca.ca>
2023-10-27 21:15:39 -0400

    ass3

--- server.js
+++ server.js
@@ -18,44 +18,47 @@ const HTTP_PORT = process.env.PORT || 8080;
 legoData.Initialize()
   .then(() => {
     app.get('/', (req, res) => {
-      res.send('Assignment 2: Your Name - Your Student Id');
+      res.sendFile(path.join(__dirname, '/views/home.html'));
     });
 
+    app.get('/about', (req, res) => {
+      res.sendFile(path.join(__dirname, '/views/about.html'));
+  });
+
     app.get('/lego/sets', (req, res) => {
-      legoData.getAllSets()
-        .then((sets) => {
-          res.json(sets);
-        })
-        .catch((error) => {
-          res.status(500).send(error);
-        });
+      const theme = req.query.theme; // Get the "theme" query parameter
+
+    if (theme) {
+        // If there is a "theme" query parameter, filter Lego data by theme
+        const filteredLegoData = legoData.filter(item => item.theme === theme);
+        if (filteredLegoData.length > 0) {
+            res.json(filteredLegoData);
+        } else {
+            res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+        }
+    } else {
+        // If there is no "theme" query parameter, respond with all Lego data
+        res.json(legoData);
+    }
     });
 
     app.get('/lego/sets/num-demo', (req, res) => {
-      const setNum = '001-1'; 
+      const setNum = req.params.set_num; // Get the set_num from the route parameter
 
-      legoData.getSetByNum(setNum)
-        .then((set) => {
-          res.json(set);
-        })
-        .catch((error) => {
-          res.status(404).send(error);
-        });
+    // Find the Lego set that matches the set_num
+    const legoSet = legoData.find(item => item.set_num === setNum);
+
+    if (legoSet) {
+        res.json(legoSet);
+    } else {
+        res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+    }
     });
 
-    app.get('/lego/sets/theme-demo', (req, res) => {
-      const theme = 'tech'; 
+    app.use((req, res) => {
+      res.status(404).sendFile(path.join(__dirname, '/views/404.html'));
+  });
 
-      legoData.getSetsByTheme(theme)
-        .then((sets) => {
-          res.json(sets);
-        })
-        .catch((error) => {
-          res.status(404).send(error);
-        });
-    });
-
-    
     app.listen(port, () => {
       console.log(`Server is running on port ${port}`);
     });
